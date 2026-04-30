8d03f24072472a0345cac642a32bb2325c032554
Anticoder03 <ap5381545@gmail.com>
2026-04-30 18:57:29 +0530

    upadte coe added inc by 2 and dec by 2 with ui

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
 import { useState } from 'react';
+import './Counter.css'
 const Counter = () => {
     // let count = 0;
     let [count,setCount] = useState(0);
     const increment = () => {
-        setCount(count + 1);
+        if (count < 10) {
+            setCount(count + 1);
+        }
+        else {
+            alert("Count should be less than 10");
+        }
+    }
+    const decrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+        else {
+            alert("Count should be greater than 0");
+        }
+    }
+    const inc_by_two = () => {
+        if (count < 9) {
+            setCount(count + 2);
+        }
+        else {
+            alert("Count should be less than 9");
+        }
+    }
+    const dec_by_two = () => {
+        if (count > 1) {
+            setCount(count - 2);
+        }
+        else {
+            alert("Count should be greater than 1");
+        }
     }
   return (
-    <div>
-      <button onClick={increment}>Click me</button>
+    <div className='counter'>
       <h1>{count}</h1>
+        <div className="buttons">
+            <div className="one_update">
+                <button className="one_button" onClick={increment}>Click me</button>
+                
+                <button className="one_button" onClick={decrement}>decrement</button>
+            
+            </div>
+            <div className="two_update">
+                <button className="two_button" onClick={inc_by_two}>increment by 2</button>
+                <button className="two_button" onClick={dec_by_two}>decrement by 2</button>
+            </div>
+             </div>
     </div>
   )
 }
